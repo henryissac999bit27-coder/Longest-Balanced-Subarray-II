eab2cff1643790e2ed4273e7e1b03267aeb88fec
Henry Issac SM <henryissac999_bit27@mepcoeng.ac.in>
2026-02-11 10:40:58 +0530

    Optimize longestBalanced method for efficiency
    Refactored longestBalanced method for improved performance by utilizing a seen array to track unique elements.

--- longestBalanced.java
+++ longestBalanced.java
@@ -20,4 +20,43 @@ class Solution {
         }
         return maxl;
     }
+
+}
+
+
+//perfect solution
+class Solution {
+    public int longestBalanced(int[] nums) {
+        int n=nums.length;
+        int maxval=0,maxl=0;
+        for(int i:nums){
+            if(i>maxval){
+                maxval=i;
+            }
+        }
+        boolean[] seen=new boolean[maxval+1];
+        for(int i=0;i<n;i++){
+            int dise=0;
+            int diso=0;
+            for (int j=i;j<n;j++){
+                int c=nums[j];
+                if(!seen[c]){
+                    seen[c]=true;
+                    if(c%2==0){
+                        dise++;
+                    }
+                    else{
+                        diso++;
+                    }
+                }               
+                if(dise==diso){
+                    maxl=Math.max(maxl,j-i+1);
+                }
+            }
+            for(int j=i;j<n;j++){
+                seen[nums[j]]=false;
+            }
+        }
+        return maxl;
+    }
 }
